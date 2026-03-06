fde72012ea91af53302487e09ac99d9d69893a06
jgpimenta <jgpimenta@yahoo.com.br>
2026-03-06 00:09:57 -0300

    Scan de segurança - versão 1.35.14

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,8 @@
 <footer class="footer">
     <div class="container">
         <div class="footer-bottom">
-            <p>&copy; <?php echo date('Y'); ?> <?php echo $empresa['nome'] ?? 'Pixel Pop'; ?>. Todos os direitos reservados. Versão 1.12</p>
+            <p>&copy; <?php echo date('Y'); ?> <?php echo $empresa['nome'] ?? 'Pixel Pop'; ?>. 
+			Todos os direitos reservados. Versão 1.12</p>
         </div>
     </div>
 </footer>
